a397bb3c3522c342732c50760763db12fa3a12ca
78Emre7878 <emre.akguel94@gmail.com>
2026-06-24 18:38:42 +0200

    Online Socket Verbindung

--- frontend/src/SocketContext.js
+++ frontend/src/SocketContext.js
@@ -1,22 +1,49 @@
 // src/SocketContext.js
-import React, { createContext, useContext, useEffect, useState } from "react";
+
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
 import { io } from "socket.io-client";
 
-export const SocketContext = createContext(null);
+export const SocketContext =
+  createContext(null);
 
-export const SocketProvider = ({ children }) => {
-  const [socket, setSocket] = useState(null);
+export const SocketProvider = ({
+  children,
+}) => {
+
+  const [socket, setSocket] =
+    useState(null);
 
   useEffect(() => {
-    const newSocket = io(); // verbindet sich automatisch mit dem Backend
+
+    const newSocket = io(
+      "https://tristano-tcg.onrender.com",
+      {
+        transports: ["websocket"],
+      }
+    );
+
     setSocket(newSocket);
 
-    return () => newSocket.close();
+    return () => {
+      newSocket.close();
+    };
+
   }, []);
 
   return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+    <SocketContext.Provider
+      value={socket}
+    >
+      {children}
+    </SocketContext.Provider>
   );
 };
 
-export const useSocket = () => useContext(SocketContext);
+export const useSocket = () =>
+  useContext(SocketContext);
